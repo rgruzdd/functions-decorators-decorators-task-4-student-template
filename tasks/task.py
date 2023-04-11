c232d603256710b0cfbdfadfb0f0d23679c328bf
Gruzin <gruzd.roman@inbox.ru>
2023-04-11 15:21:20 +0300

    Commit

--- tasks/task.py
+++ tasks/task.py
@@ -1,10 +1,17 @@
 
-def validate():
-    '''
-    Add corresponded arguments and implementation here. 
-    '''
-    pass 
+def validate(fanc):
+    def wrapper(*args, **kwargs):
+        result = fanc(*args, **kwargs)
+        for i in args:
+            if i > 256:
+                result_string = "Function call is not valid!"
+                return result_string
+        result_string = result
+        return result_string
+    return wrapper
 
 @validate
 def set_pixel(x: int, y: int, z: int) -> str:
-  return "Pixel created!"
+    return "Pixel created!"
+
+
